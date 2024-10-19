f956b255021a10980c946efdf18d7970266c22df
VagishG <vagish.study@gmail.com>
2024-10-19 14:16:17 +0530

    Completed Task3

---
+++ app/dashboard/form/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import FormComponent from "@/components/dashboard/form";
+export default function page() {
+  return (
+    <div>
+      <FormComponent />
+    </div>
+  );
+}
